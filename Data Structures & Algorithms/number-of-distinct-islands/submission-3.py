869a3030c08b7e8675018ec1f332d3861776433a
Lucca Boas <86315612+Luccacvb@users.noreply.github.com>
2026-07-11 14:59:21 -0300

    Add: number-of-distinct-islands - submission-3

---
+++ Data Structures & Algorithms/number-of-distinct-islands/submission-3.py
@@ -0,0 +1,40 @@
+class Solution:
+    def _dfs(
+        self,
+        row: int,
+        col: int,
+        start_row: int,
+        start_col: int,
+        grid: List[List[int]],
+        shape: List[Tuple[int, int]],
+    ) -> None:
+        if row < 0 or col < 0:
+            return
+
+        if row >= len(grid) or col >= len(grid[0]):
+            return
+
+        if grid[row][col] == 0:
+            return
+
+        grid[row][col] = 0
+        shape.append((row - start_row, col - start_col))
+
+        self._dfs(row, col + 1, start_row, start_col, grid, shape)
+        self._dfs(row + 1, col, start_row, start_col, grid, shape)
+        self._dfs(row, col - 1, start_row, start_col, grid, shape)
+        self._dfs(row - 1, col, start_row, start_col, grid, shape)
+
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        seen_shapes = set()
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    shape = []
+                    self._dfs(row, col, row, col, grid, shape)
+                    seen_shapes.add(tuple(shape))
+
+        return len(seen_shapes)

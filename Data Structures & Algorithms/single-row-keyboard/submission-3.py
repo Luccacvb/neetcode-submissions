e1e1e003cb31d42261ea9c15f207303f06d003bf
Lucca Boas <luccavilasboas@hotmail.com>
2026-08-27 21:00:05 -0300

    Add: single-row-keyboard - submission-3

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        mapp = {}
+        for i, k in enumerate(keyboard):
+            mapp[k] = i
+    
+        count = 0
+        j = 0
+        for w in word:
+            find_position = mapp[w]
+            count += abs(j - find_position)
+            j = find_position
+        
+        return count

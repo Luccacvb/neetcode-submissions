9282ffc70845a633abc43c6d6a5afad6647cc919
Lucca Boas <luccavilasboas@hotmail.com>
2026-08-30 20:57:52 -0300

    Add: group-shifted-strings - submission-0

---
+++ Data Structures & Algorithms/group-shifted-strings/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        groups = {}
+
+        for string in strings :
+            signature = []
+
+            for i in range(len(string) - 1):
+                char1 = string[i]
+                char2 = string[i + 1]
+                difference = (ord(char2) - ord(char1)) % 26
+                signature.append(difference)
+        
+            key = tuple(signature)
+            if key not in groups:
+                groups[key] = []
+
+            groups[key].append(string)
+        
+        return list(groups.values())
